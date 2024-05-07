12e3f5c4e4b6093148c36759cf05c78553943b9b
sangeeth <ssulochanaherath@gmail.com>
2024-05-07 20:23:03 +0530

    Completed case_01.

---
+++ case_01/case_01.js
@@ -0,0 +1,18 @@
+var array = ["A","B","C","D","E","F"];
+
+var myInterval = null;
+
+function replaceValues() {
+
+    let lastValue = array.pop();
+    array.unshift(lastValue);
+
+    $("#div1").text(array[0]);
+    $("#div2").text(array[1]);
+    $("#div3").text(array[2]);
+    $("#div4").text(array[3]);
+    $("#div5").text(array[4]);
+    $("#div6").text(array[5]);
+}
+
+setInterval(replaceValues, 800);
